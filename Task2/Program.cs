fc52c5d8eef9b2c5a19131d72a32ea29f9af55a1
Gorelkina Katya <teterkinaejaterina@gmail.com>
2023-05-26 14:49:57 +0300

    done task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите 1-е число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-е число: ");
+int m = int.Parse(Console.ReadLine()!);
+if (n>m)
+    Console.WriteLine($"Наибольшее число {n}\nНаименьшее число {m}");
+else if (n<m)
+    Console.WriteLine($"Наибольшее число {m}\nНаименьшее число {n}");
+else
+    Console.WriteLine($"Числа равны");
