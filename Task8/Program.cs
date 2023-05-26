05b729dcdd6e1a004f10fd19680a2255465131b6
Gorelkina Katya <teterkinaejaterina@gmail.com>
2023-05-26 15:25:35 +0300

    done task 8

---
+++ Task8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите положительное число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+while (n <= 0)
+{
+    Console.Write("Вы ошиблись!\nВведите положительное число: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+
+for (int i = 2; i <= n; i+=2)
+    Console.Write($"{i} ");
