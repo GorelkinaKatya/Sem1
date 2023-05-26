a430f0343f4c14d8028a592632093d49f7b18523
Gorelkina Katya <teterkinaejaterina@gmail.com>
2023-05-26 15:08:04 +0300

    done task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите 1-е число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-е число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 3-е число: ");
+int p = int.Parse(Console.ReadLine()!);
+
+if ( n > m && n > p)
+    Console.WriteLine($"Наибольшее число {n}");
+else if (m > n && m > p)
+    Console.WriteLine($"Наибольшее число {m}");
+else
+    Console.WriteLine($"Наибольшее число {p}");
